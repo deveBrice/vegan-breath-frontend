fa6aaa365892d4c4135ab78f7f5fa39b4c4f0628
deveBrice <42933211+deveBrice@users.noreply.github.com>
2021-01-07 23:01:07 +0100

    Update server.js
    Update server.js file

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(src + '/dist/breath-vegan-front'));
+app.use(express.static(__dirname + '/dist/breath-vegan-front'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join('index.html, {root: 'dist/breath-vegan-front/’}'));
+    res.sendFile('index.html', {root: 'dist/breath-vegan-front/'});
 });
 
 // Start the app by listening on the default Heroku port.
